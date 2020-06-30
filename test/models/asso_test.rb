8267d902c59db8e548709905ca97fcc5d5f63364
Pierre Garcia <pg.pierre.garcia@free.fr>
2020-07-01 01:28:45 +0200

    install pundit policies

--- test/models/asso_test.rb
+++ test/models/asso_test.rb
@@ -1,6 +1,7 @@
 require 'test_helper'
 
 class AssoTest < ActiveSupport::TestCase
+  self.use_instantiated_fixtures = true
 
   def setup
     @user = User.new(pseudo: 'test', email:'test@gmail.com',
@@ -10,6 +11,11 @@ class AssoTest < ActiveSupport::TestCase
   end
 
   test "should have a REGEX register to be valid" do
+  end
+
+  test "should not be valid without register" do
+    @asso.register = ''
+    assert_not @asso.valid?
   end
 
   test "name should not be too long" do
@@ -23,10 +29,10 @@ class AssoTest < ActiveSupport::TestCase
     assert @asso.valid?
   end
 
-  # test "should not be valid if created from a non recorded user" do
-  #   @asso.user = @user
-  #   assert @asso.valid?
-  # end
+  test "should not be valid if created from a non recorded user" do
+    @asso.user = @user
+    assert @asso.valid?
+  end
 
   test "should be saved in Asso database" do
     @asso.user = @user
