a0390e09e274adb8a297f74b918ad142cd67bb38
Pierre Garcia <pg.pierre.garcia@free.fr>
2020-07-01 09:55:10 +0200

    user and asso policies for index show new create

---
+++ app/policies/asso_policy.rb
@@ -0,0 +1,24 @@
+class AssoPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+  def index
+    true
+  end
+
+  def show
+    true
+  end
+
+  def new
+    !!user
+  end
+
+  def create
+    # record.user = user
+  end
+
+end
